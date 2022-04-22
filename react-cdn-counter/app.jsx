bc27ed41ce607b7dbf489152122d14068b1d14ec
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-04-22 10:31:16 +0200

    Update app.jsx

--- react-cdn-counter/app.jsx
+++ react-cdn-counter/app.jsx
@@ -2,31 +2,31 @@ import React, { useState } from "https://cdn.skypack.dev/react";
 import ReactDOM from "https://cdn.skypack.dev/react-dom";
 
 function Greeting({ name }) {
-  return <h1>Hello, {name}</h1>;
+    return <h1>Hello, {name}</h1>;
 }
 
 function Counter() {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-  function updateCount() {
-    setCount(count + 1);
-  }
+    function updateCount() {
+        setCount(count + 1);
+    }
 
-  return (
-    <section>
-      My count: <p>{count}</p>
-      <button onClick={updateCount}>Increment Count</button>
-    </section>
-  );
+    return (
+        <section>
+            My count: <p>{count}</p>
+            <button onClick={updateCount}>Increment Count</button>
+        </section>
+    );
 }
 
 function App() {
-  return (
-    <>
-      <Greeting name='world' />
-      <Counter />
-    </>
-  );
+    return (
+        <>
+            <Greeting name="world" />
+            <Counter />
+        </>
+    );
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
