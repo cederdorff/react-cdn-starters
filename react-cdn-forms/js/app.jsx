3af41a9e9849e0b051f69ff6723b615c0192d1dd
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-04-27 13:23:45 +0200

    Update app.jsx

--- react-cdn-forms/js/app.jsx
+++ react-cdn-forms/js/app.jsx
@@ -39,11 +39,51 @@ function Form() {
     );
 }
 
+function Form2() {
+    const [javaScript, setJavaScript] = useState(localStorage.getItem("JavaScript"));
+    const [html, setHtml] = useState(localStorage.getItem("HTML"));
+    const [css, setCss] = useState(localStorage.getItem("CSS"));
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        for (const item of event.target.elements) {
+            if (item.checked) {
+                localStorage.setItem(item.value, true);
+            } else {
+                localStorage.removeItem(item.value);
+            }
+        }
+    }
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    JavaScript
+                    <input type="checkbox" value="JavaScript" onChange={e => setJavaScript(e.target.checked)} checked={javaScript} />
+                </label>
+                <label>
+                    HTML
+                    <input type="checkbox" value="HTML" onChange={e => setHtml(e.target.checked)} checked={html} />
+                </label>
+                <label>
+                    CSS
+                    <input type="checkbox" value="CSS" onChange={e => setCss(e.target.checked)} checked={css} />
+                </label>
+
+                <button>submit</button>
+            </form>
+        </>
+    );
+}
+
 function App() {
     return (
         <>
             <h1>Hello Form</h1>
-            <Form />
+            {/* <Form /> */}
+            <Form2 />
         </>
     );
 }
